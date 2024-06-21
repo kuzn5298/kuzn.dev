0df15de79495ab6869ce7b158274a6c9265391c7
Nikita Kuznetsov <kuzn5298@gmail.com>
2024-06-22 01:44:36 +0200

    🧪 test 2

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -13,13 +13,13 @@ export const wrapPageElement = PageWrapper;
 
 export const replaceHydrateFunction = () => (element, container) => {
     // ? Problem with render element in wrapPageElement (element is null in ssr)
-    const useHydrate = container && container.children.length;
+    // const useHydrate = container && container.children.length;
 
-    if (useHydrate) {
-        const root = ReactDOM.hydrateRoot(container, element);
-        return () => root.unmount();
-    }
+    // if (useHydrate) {
+    //     const root = ReactDOM.hydrateRoot(container, element);
+    //     return () => root.unmount();
+    // }
     const root = ReactDOM.createRoot(container);
     root.render(element);
-    return () => root.unmount();
+    // return () => root.unmount();
 };
