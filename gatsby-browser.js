6ab30775159d974a249f7cea9f6f792a4620a1be
Nikita Kuznetsov <kuzn5298@gmail.com>
2024-06-22 01:31:42 +0200

    🧹 Remove hydrate options

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,8 +4,22 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
  */
 
+import * as ReactDOM from 'react-dom/client';
 import { PageWrapper, RootWrapper } from '@components/app';
 
 export const wrapRootElement = RootWrapper;
 
 export const wrapPageElement = PageWrapper;
+
+export const replaceHydrateFunction = () => (element, container) => {
+    // ? Problem with render element in wrapPageElement (element is null in ssr)
+    // const useHydrate = container && container.children.length;
+
+    // if (useHydrate) {
+    //     const root = ReactDOM.hydrateRoot(container, element);
+    //     return () => root.unmount();
+    // }
+    const root = ReactDOM.createRoot(container);
+    root.render(element);
+    return () => root.unmount();
+};
